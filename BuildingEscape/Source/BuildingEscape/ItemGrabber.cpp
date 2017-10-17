9507baace75c5c05f70ac69a99d02424c93b56fa
Cristian Fabbri <cristian.fabbri5@studio.unibo.it>
2017-10-17 17:49:52 +0200

    New DeafultPawn_BP with ItemGrabber

---
+++ BuildingEscape/Source/BuildingEscape/ItemGrabber.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ItemGrabber.h"
+
+
+// Sets default values for this component's properties
+UItemGrabber::UItemGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UItemGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UItemGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
